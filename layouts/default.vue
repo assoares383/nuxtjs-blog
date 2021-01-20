ec93c586bb1b6532d175b9f534b11f48acd80ec5
assoares <alexandressoares383@gmail.com>
2021-01-20 10:46:18 -0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSidenav :show="displaySidenav" @close="displaySidenav = false" />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import TheHeader from '../components/Navigation/TheHeader';
+import TheSidenav from '../components/Navigation/TheSidenav';
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySidenav: false
+    }
+  }
+}
+
+</script>
+
+<style>
+html, body {
+  font-family: 'Open Sans', sans-serif;
+  margin: 0;
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <PostPreview
+        id="1"
+        thumbnail="https://images.wallpaperscraft.com/image/code_programming_symbols_140997_1920x1080.jpg"
+        title="Hello There!"
+        previewText="This my first post"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://images.wallpaperscraft.com/image/code_programming_symbols_140997_1920x1080.jpg"
+        title="Hello There!"
+        previewText="This my first post"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://images.wallpaperscraft.com/image/code_programming_symbols_140997_1920x1080.jpg"
+        title="Hello There!"
+        previewText="This my first post"
+      />
+      <PostPreview
+        id="4"
+        thumbnail="https://images.wallpaperscraft.com/image/code_programming_symbols_140997_1920x1080.jpg"
+        title="Hello There!"
+        previewText="This my first post"
+      />
+    </section>
+  </div>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview';
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+  .intro {
+    position: relative;
+    padding: 0;
+    box-sizing: border-box;
+    background-position: center;
+    background-size: cover;
+    max-height: 300px;
+    height: 100vh;
+    background-color: rgb(211, 211, 211);
+    box-shadow: 0 3px 10px rgb(71, 71, 71);
+    display: flex;
+    align-items: center;
+  }
+
+  .intro h1 {
+    width: 100%;
+    font-size: 1.5rem;
+    color: black;
+    padding: 10px;
+    border-radius: 0;
+    border: none;
+    margin: 0;
+    text-align: center;
+  }
+
+  @media (min-width: 768px) {
+    .intro h1 {
+      font-size: 2rem;
+    }
+  }
+
+  .featured-posts {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
