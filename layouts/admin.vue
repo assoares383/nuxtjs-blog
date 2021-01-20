6f2b0db12e51a71ae83893faedf536a71fd67ddd
assoares <alexandressoares383@gmail.com>
2021-01-20 14:00:31 -0300

    adding a separate layout to the admin area

---
+++ layouts/admin.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html, body {
+  font-family: 'Open Sans', sans-serif;
+  margin: 0;
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
